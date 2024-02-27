8f0160ba7f8a01b930267914159e6393f854b9c8
นรภัทร หนูทอง <noorrapat1247@gmail.com>
2024-02-28 00:23:20 +0700

    add transaction page

--- payment/client/src/components/transaction.jsx
+++ payment/client/src/components/transaction.jsx
@@ -1,6 +1,8 @@
 import './transaction.css'
 import React, {useState} from "react";
 import NavigateBar from "./Navbar"
+import Qrcode from "../Source/Qrcode.png";
+import { MdOutlineUploadFile } from "react-icons/md";
 
 function Transaction(){
 
@@ -9,6 +11,26 @@ function Transaction(){
             <NavigateBar/>
             <div className='body'>
                 <div className='container'>
+                
+                    <div>
+                        <div style={{textAlign:'center'}}><label>วิธีการชำระเงิน</label></div>
+                        <img src={Qrcode} width='60%' height='60%'/>
+                        <div style={{display:'flex'}}>
+                            <div className='cost-box'>
+                                <div><label className='kanit-medium'>ราคารวม xxx</label></div>
+                                <div><label className='kanit-medium'>จำนวนคน xxx</label></div>
+                                <div><label className='kanit-medium-highlight'>ราคาสุทธิ xxx</label></div>
+                            </div>
+                            <div className='cost-box-2'>
+                                <div><label className='kanit-medium' style={{float:'right'}}>เเจ้งการโอนเงิน</label></div>
+                                <div>
+                                    <button className='cost-button'> <MdOutlineUploadFile/> อัปโหลดสลิปหลักฐานการโอน</button>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
                     
                     <div className='box'>
                         <a href='http://localhost:3000/'className='kanit-medium'>ย้อนกลับ</a>

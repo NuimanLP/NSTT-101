9c559be74c0056680e00dc793056de4a192c9429
Chavatik <6610110066@psu.ac.th>
2024-03-08 19:22:47 +0700

    Fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,12 +1,12 @@
 const isProd = process.env.NODE_ENV === 'production'
 
-export const config = {
+const config = {
   isProd,
   serverUrlPrefix: isProd ? 'https://wd01.cloud-workshop.online/api' : 'http://localhost:1337/api',
   serverReceipt: isProd ? 'https://wd01-admin.cloud-workshop.online' : 'http://localhost:1337',
-  windowlocateHome: isProd ? 'https://wd01.cloud-workshop.online' : 'http://localhost:3000'
+  windowlocateHome: isProd ? 'https://wd01.cloud-workshop.online' : 'http://localhost:3000',
+  ForSpecial: isProd ? 'https://wd01-admin.cloud-workshop.online' : 'http://localhost:1337',
 }
-export const config2 = {
-  isProd,
-  serverUrlPrefix: isProd ? 'https://wd01-admin.cloud-workshop.online/api' : 'http://localhost:1337/api',}
+
+export default config;
 
